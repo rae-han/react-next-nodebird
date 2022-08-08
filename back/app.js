16a8fb0b1799f84e4db4485173800442dffdf10b
raehan <raehan900809@gmail.com>
2022-08-08 13:48:34 +0900

    add: environment add: packages for secure with hpp, helmet

--- back/app.js
+++ back/app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -25,6 +27,14 @@ db.sequelize.sync()
 passportConfig();
 dotenv.config();
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp())
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
+
 // use는 express서버에 뭔가를 장착한다는 뜻.
 app.use(express.json()); // 이건 프론트에서 json형식으로 데이터를 보냈을때 req.body에 넣겠다.
 // 즉 axios로 보내면 여기로 온다.
@@ -32,7 +42,8 @@ app.use(express.urlencoded({ extended: true })); // 폼서브밋으로 데이터
 // 일반 form 으로 보내면 여기로 온다.
 app.use(cors({
   // origin: '*',
-  origin: true, // 위는 모든 주소, 이건 보낸 곳의 주소가 자동으로 들어간다.
+  // origin: true, // 위는 모든 주소, 이건 보낸 곳의 주소가 자동으로 들어간다.
+  origin: ['http://localhost:3060', 'nodebird.com'], // 위는 모든 주소, 이건 보낸 곳의 주소가 자동으로 들어간다.
   credentials: true,
   secret: process.env.COOKIE_SECRET, // 서버에서 특정 정보에 해당하는 id를 암호화된 문자열로 바꿔서 클라이언트에 보내준다고 했는데 그때 암호화할 때 사용된다. 그래서 잘 숨겨야한다.
 }))
@@ -40,10 +51,10 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   saveUninitialized: false,
   resave: false,
+  secret: process.env.COOKIE_SECRET,
 }));
 app.use(passport.initialize())
 app.use(passport.session());
-app.use(morgan('dev'))
 // static
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 // path.join 을 쓰는 이유가 윈도우와 맥 리눅스의 경로 구분자가 다르기 때문에.
